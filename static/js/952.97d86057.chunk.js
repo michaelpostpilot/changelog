97445f0800279147ddc0f29ab2eb67005c787cfd
Michael Epstein <micha@MacBook-Air-7.attlocal.net>
2025-03-14 16:13:54 -0700

    Updates

---
+++ static/js/952.97d86057.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchangelog_app=self.webpackChunkchangelog_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.97d86057.chunk.js.map
